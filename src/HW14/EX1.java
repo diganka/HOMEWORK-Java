144daf396a9d64deef4b432629643e2ace2c50f2
Diana <dianaturek95@gmail.com>
2023-03-05 19:19:27 +0100

    added exercise 1

---
+++ src/HW14/EX1.java
@@ -0,0 +1,13 @@
+package HW14;
+
+public class EX1 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 9;
+        a+=b;
+        b=a-b;
+        a-=b;
+        System.out.println("a= "+a+" b="+b);
+        }
+    }
