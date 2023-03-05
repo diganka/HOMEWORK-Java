df357543c54104e4bd00339f7b41fe97d791e139
Diana <dianaturek95@gmail.com>
2023-03-05 21:01:40 +0100

    added exercise 2

---
+++ src/HW14/EX2.java
@@ -0,0 +1,14 @@
+package HW14;
+
+public class EX2 {
+    public static void main(String[] args) {
+        double chislitel = 7.0;
+        double znamenatel = 5.0;
+        double d=(chislitel/znamenatel);
+        int x=(int) d;
+        int y=(int) (chislitel%znamenatel);
+        int z=(int) (znamenatel);
+        System.out.println("Целая часть равна - "+x+"\nОстаток от деления равен - "+y+"\nЗнаменатель равен - "+ z);
+
+    }
+}
